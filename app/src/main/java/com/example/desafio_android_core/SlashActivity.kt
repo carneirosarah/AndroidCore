b8ae2b67a7640ffaa9e9e056057228c322d6eb16
Sarah Ribeiro Lisboa <sarahrlcarneiro@gmail.com>
2020-11-11 19:42:11 -0300

    configurações iniciais

---
+++ app/src/main/java/com/example/desafio_android_core/SlashActivity.kt
@@ -0,0 +1,11 @@
+package com.example.desafio_android_core
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SlashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_slash)
+    }
+}
