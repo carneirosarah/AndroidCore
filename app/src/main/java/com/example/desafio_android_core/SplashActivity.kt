a3abd112192301f56f857284371bd9b4d4c70933
Sarah Ribeiro Lisboa <sarahrlcarneiro@gmail.com>
2020-11-11 20:39:13 -0300

    Tela de Login - Implementação inicial

--- app/src/main/java/com/example/desafio_android_core/SplashActivity.kt
+++ app/src/main/java/com/example/desafio_android_core/SplashActivity.kt
@@ -13,6 +13,6 @@ class SplashActivity : AppCompatActivity() {
         Handler().postDelayed({
 
             startActivity(Intent(this, LoginActivity::class.java))
-        }, 3000)
+        }, 6000)
     }
 }
