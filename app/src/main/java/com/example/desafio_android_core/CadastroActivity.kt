9764379d7b86e8b48f629d02d020fa8be9bcdb53
Sarah Ribeiro Lisboa <sarahrlcarneiro@gmail.com>
2020-11-11 23:33:23 -0300

    Implementação tela de cadastro

--- app/src/main/java/com/example/desafio_android_core/CadastroActivity.kt
+++ app/src/main/java/com/example/desafio_android_core/CadastroActivity.kt
@@ -1,11 +1,21 @@
 package com.example.desafio_android_core
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_cadastro.*
 
 class CadastroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro)
+
+        topAppBar.setNavigationOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        registerButton.setOnClickListener{
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
