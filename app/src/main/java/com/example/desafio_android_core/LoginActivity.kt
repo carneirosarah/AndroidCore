05a7a57921a2e244d80af023e71e2fac198bd7a4
Sarah Ribeiro Lisboa <sarahrlcarneiro@gmail.com>
2020-11-11 23:06:50 -0300

    Implementação da tela de login

--- app/src/main/java/com/example/desafio_android_core/LoginActivity.kt
+++ app/src/main/java/com/example/desafio_android_core/LoginActivity.kt
@@ -1,11 +1,22 @@
 package com.example.desafio_android_core
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        loginButton.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+
+        loginRegisterButton.setOnClickListener {
+            startActivity(Intent(this, CadastroActivity::class.java))
+        }
     }
 }
